5753849d6d1a9823578c144e7a020a961735611e
Borja Fourquet <b.fourquet@outlook.es>
2015-11-08 21:08:16 +0100

    Commit inicial con el resto de archivos y directorios

---
+++ js/character.js
@@ -0,0 +1,168 @@
+
+Character = function (index, x, y, game, player) {
+	this.cursor = {
+		left:false,
+		right:false,
+		up:false,
+		fire:false		
+	}
+
+	this.input = {
+		left:false,
+		right:false,
+		up:false,
+		fire:false
+	}
+
+
+    this.game = game;
+    this.health = 30;
+    this.player = player;
+    /* this.bullets = game.add.group();
+    this.bullets.enableBody = true;
+    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    this.bullets.createMultiple(20, 'bullet', 0, false);
+    this.bullets.setAll('anchor.x', 0.5);
+    this.bullets.setAll('anchor.y', 0.5);
+    this.bullets.setAll('outOfBoundsKill', true);
+    this.bullets.setAll('checkWorldBounds', true);	 */
+	
+	
+	this.currentSpeed =0;
+    this.fireRate = 100;
+    this.nextFire = 0;
+    this.alive = true;
+    this.jumpCooldown = 350;
+
+    //this.shadow = game.add.sprite(x, y, 'enemy', 'shadow');
+    this.character = game.add.sprite(x, y, 'player');
+    this.character.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.character.animations.add('right', [5, 6, 7, 8], 10, true);
+    
+    game.spacePhysics.addDynamic(this.character);
+
+    //this.shadow.anchor.set(0.5);
+    this.character.anchor.set(0.5);
+    this.character.scale.setTo(0.65,0.65);
+    //this.turret.anchor.set(0.3, 0.5);
+
+    this.character.id = index;
+    game.physics.box2d.enable(this.character);
+    //game.physics.enable(this.tank, Phaser.Physics.ARCADE);
+    this.character.body.immovable = false;
+    this.character.body.collideWorldBounds = true;
+    
+
+    //this.tank.angle = 0;
+
+//game.physics.arcade.velocityFromRotation(this.tank.rotation, 0, this.tank.body.velocity);
+
+};
+
+Character.prototype.update = function() {
+	
+    
+    // TODO
+	var inputChanged = (
+		this.cursor.left != this.input.left ||
+		this.cursor.right != this.input.right ||
+		this.cursor.up != this.input.up ||
+		this.cursor.fire != this.input.fire
+	);
+	
+    inputChanged = true;
+    inputChanged = true;
+	if (inputChanged)
+	{
+		//Handle input change here
+		//send new values to the server		
+		if (this.character.id == myId)
+		{
+			// send latest valid state to the server
+			this.input.x = this.character.x;
+			this.input.y = this.character.y;
+			this.input.angle = this.character.angle;
+			this.input.rot = this.character.angle;
+			
+            console.log("Updating character " + this.character.id)
+			
+            // NO FUNCIONA
+			eurecaServer.handleKeys(this.input);
+			
+		}
+	}
+
+	//cursor value is now updated by eurecaClient.exports.updateState method
+	
+	
+    if (this.cursor.left)
+    {
+        this.character.angle -= 1;
+    }
+    else if (this.cursor.right)
+    {
+        this.character.angle += 1;
+    }	
+    if (this.cursor.up)
+    {
+        //  The speed we'll travel at
+        this.currentSpeed = 300;
+    }
+    else
+    {
+        if (this.currentSpeed > 0)
+        {
+            this.currentSpeed -= 4;
+        }
+    }
+    if (this.cursor.fire)
+    {	
+		this.fire({x:this.cursor.tx, y:this.cursor.ty});
+    }
+	
+	
+	
+    if (this.currentSpeed > 0)
+    {
+        //game.physics.arcade.velocityFromRotation(this.tank.rotation, this.currentSpeed, this.tank.body.velocity);
+    }	
+	else
+	{
+		//game.physics.arcade.velocityFromRotation(this.tank.rotation, 0, this.tank.body.velocity);
+	}
+	
+	
+	
+	
+    //this.shadow.x = this.character.x;
+    //this.shadow.y = this.character.y;
+    //this.shadow.rotation = this.tank.rotation;
+
+    //this.turret.x = this.tank.x;
+    //this.turret.y = this.tank.y;
+};
+
+// De momento no la usamos
+Character.prototype.fire = function(target) {
+		if (!this.alive) return;
+        if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0)
+        {
+            this.nextFire = this.game.time.now + this.fireRate;
+            var bullet = this.bullets.getFirstDead();
+            bullet.reset(this.turret.x, this.turret.y);
+
+			bullet.rotation = this.game.physics.arcade.moveToObject(bullet, target, 500);
+        }
+}
+
+
+Character.prototype.kill = function() {
+	this.alive = false;
+	this.character.kill();
+	//this.turret.kill();
+	//this.shadow.kill();
+}
+
+Character.prototype.toString = function () {
+    return "Player id: " + this.character.id + "\n HP : " + this.health;
+}
---
+++ js/spacePhysics.js
@@ -0,0 +1,111 @@
+
+
+/**
+ *
+ * @
+ * @file spacePhysics.js
+ * @date 07/11/2015
+ */
+
+function SpacePhysics(game){
+    this.game = game;
+    
+    // groups containing crates and planets
+
+    this.dynamicGroup = game.add.group();
+    this.planetGroup = game.add.group();
+    this.planet;
+    
+    //flag for gameover and limit of crates
+    this.gameover = 0;
+    this.numCrates = 0;
+    this.numCollision = 0;
+    this.x = -1;
+    this.y = -1;
+    
+    this.grounded = false;
+
+    // a force reducer to let the simulation run smoothly
+
+    this.forceReducer = 0.015;
+
+    // graphic object where to draw planet gravity area
+
+    this.gravityGraphics;
+    this.sprite;
+    this.debug = false;
+
+
+}
+
+SpacePhysics.prototype.consoleLog = function() {
+    console.log("@Physics: " + dynamicGroup);
+};
+
+SpacePhysics.prototype.addPlanet = function (posX, posY, gravityRadius, gravityForce, asset) {
+    	planet = game.add.sprite(posX, posY, asset);
+        planet.anchor.setTo(0.5, 0.5)
+        planet.gravityRadius = gravityRadius;
+        planet.gravityForce = gravityForce;
+        this.planetGroup.add(planet);
+        game.physics.box2d.enable(planet);
+        planet.body.static = true;
+
+        // look how I create a circular body
+
+        planet.body.setCircle(planet.width / 2);
+        gravityGraphics.drawCircle(planet.x, planet.y,                   planet.width+planet.gravityRadius);
+        if(this.debug) console.log("@Physics: " + "planet created ->" + planet);
+        return planet;
+}
+
+SpacePhysics.prototype.isGrounded = function isGrounded(body){    
+    var vs = 25
+    return (Math.abs(body.velocity.x) < vs) && (Math.abs(body.velocity.y) < vs)
+}
+
+
+/**
+ * @
+ * Añade un objeto al grupo de elementos dinámicos del motor
+ */
+
+SpacePhysics.prototype.addDynamic = function (body){
+    this.dynamicGroup.add(body)
+}
+
+SpacePhysics.prototype.update = function (){    
+        // Para cada elemento dinámico
+		for(var i=0;i<this.dynamicGroup.total;i++){
+			var c = this.dynamicGroup.getChildAt(i);			
+			// Para cada planeta (generador de gravedad)			        
+            if (c.body.immovable) continue;
+			for(var j=0;j<this.planetGroup.total;j++){ 
+				var p = this.planetGroup.getChildAt(j);				
+				// Distance entre dos puntos				
+				var distance = Phaser.Math.distance(c.x,c.y,p.x,p.y);				
+				// Si la distancia es menor que el radio de acción del planeta
+				x = c.x;
+				y = c.y;				
+				if(distance<p.width/2+p.gravityRadius/2){					
+					// calculating angle between the planet and the crate					
+					var angle = Phaser.Math.angleBetween(c.x,c.y,p.x,p.y);					
+					// add gravity force to the crate in the direction of planet center					
+					c.body.applyForce(p.gravityForce*Math.cos(angle)*this.forceReducer,p.gravityForce*Math.sin(angle)*this.forceReducer);
+                }
+            }
+        }
+}
+                
+
+
+
+
+
+
+
+
+
+
+
+

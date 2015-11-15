e154d9e3568b251839af76fe3e22c9ef8589fae7
eduardo radio gallego <eduardo radio gallego>
2015-11-15 20:43:58 +0100

    Preloader con estrellas

--- public/js/states/Preloader.js
+++ public/js/states/Preloader.js
@@ -4,28 +4,73 @@ GALACTIC_STRIKE.Preloader = function(game) {
 
 GALACTIC_STRIKE.Preloader.prototype = {
 
-    preload: function() {
+function preload() {
 
-    },
+    game.load.image('star', 'assets/estrella.jpg');
 
-    create: function() {
-        this.game.stage.backgroundColor = '#ffffff';
-        var logo = this.add.sprite(this.world.width/2, this.world.height/2, 'appsbude');
-        logo.anchor.set(0.5, 0.5);
+}
 
-        this.game.time.events.add(Phaser.Timer.SECOND * 2.0, function() {
+var distance = 300;
+var speed = 4;
+var stars;
 
-            var tween = this.add.tween(logo)
-                .to({alpha: 0}, 750, Phaser.Easing.Linear.none);
+var max = 200;
+var xx = [];
+var yy = [];
+var zz = [];
 
-            tween.onComplete.add(function() {
-                logo.destroy();
-                this.startGame();
-            }, this);
+function create() {
 
-            tween.start();
-        }, this);
-    },
+    if (game.renderType === Phaser.WEBGL)
+    {
+        max = 2000;
+    }
+
+    var sprites = game.add.spriteBatch();
+
+    stars = [];
+
+    for (var i = 0; i < max; i++)
+    {
+        xx[i] = Math.floor(Math.random() * 800) - 400;
+        yy[i] = Math.floor(Math.random() * 600) - 300;
+        zz[i] = Math.floor(Math.random() * 1700) - 100;
+
+        var star = game.make.sprite(0, 0, 'star');
+        star.anchor.set(0.5);
+
+        sprites.addChild(star);
+
+        stars.push(star);
+    }
+         var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        // When the 'upKey' is pressed, it will call the 'start' function once
+        upKey.onDown.addOnce(this.startGame, this);
+
+}
+
+function update() {
+
+    for (var i = 0; i < max; i++)
+    {
+        stars[i].perspective = distance / (distance - zz[i]);
+        stars[i].x = game.world.centerX + xx[i] * stars[i].perspective;
+        stars[i].y = game.world.centerY + yy[i] * stars[i].perspective;
+
+        zz[i] += speed;
+
+        if (zz[i] > 290)
+        {
+            zz[i] -= 600;
+        }
+
+        stars[i].alpha = Math.min(stars[i].perspective / 2, 1);
+        stars[i].scale.set(stars[i].perspective / 2);
+        stars[i].rotation += 0.1;
+
+    }
+
+},
 
     startGame: function() {
         this.state.start('MainMenu');

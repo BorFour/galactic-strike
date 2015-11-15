d07d7dda687265c402d1c3bc3f51565c373f4f12
Borja Fourquet <BorFour@bitbucket.org>
2015-11-15 19:41:10 +0100

    States

--- public/js/states/Game.js
+++ public/js/states/Game.js
@@ -1,21 +1,13 @@
-GALACTIC_STRIKE.Game = function(game) {
 
-};
+//	100% of the browser window - see Boot.js for additional configuration
+var SAFE_ZONE_WIDTH = 960;// * window.devicePixelRatio;
+var SAFE_ZONE_HEIGHT = 640;// * window.devicePixelRatio;
+var game = new Phaser.Game(SAFE_ZONE_WIDTH, SAFE_ZONE_HEIGHT, Phaser.AUTO, '');
+//	Add the States your game has.
 
-GALACTIC_STRIKE.Game.prototype = {
-	create: function() {
-
-	},
-
-	update: function() {
-
-	},
-
-	quitGame: function(pointer) {
-		this.state.start('MainMenu');
-	},
-
-	render: function() {
-
-	}
-};
+game.state.add('Boot', GALACTIC_STRIKE.Boot);
+game.state.add('Preloader', GALACTIC_STRIKE.Preloader);
+game.state.add('MainMenu', GALACTIC_STRIKE.MainMenu);
+game.state.add('Play', GALACTIC_STRIKE.Game);
+//	Now start the Boot state.
+game.state.start('Boot');
---
+++ public/js/states/play.js
@@ -0,0 +1,21 @@
+GALACTIC_STRIKE.Play = function(game) {
+
+};
+
+GALACTIC_STRIKE.Play.prototype = {
+	create: function() {
+
+	},
+
+	update: function() {
+
+	},
+
+	quitGame: function(pointer) {
+		this.state.start('MainMenu');
+	},
+
+	render: function() {
+
+	}
+};

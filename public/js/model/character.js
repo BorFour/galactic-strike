fcbe635326fdb8dae3c85e1218dfec755d108cbb
eduardo radio gallego <eduardo radio gallego>
2015-12-31 19:02:12 +0100

    Character.js comentado y limpio

--- public/js/model/character.js
+++ public/js/model/character.js
@@ -4,17 +4,13 @@ Character.prototype.constructor = Element;
 
 function Character(x, y, angle, game, player, asset)
 {
-
     Element.call(this, game, x, y, asset);
-    //game.add.existing(this);
 
     this.rotation = angle;
-
     this.player = player;
     this.game = game;
-    this.debug = false;
 
-    //    this.CG_character = game.physics.p2.createCollisionGroup(); //COLLISION GROUP
+    this.debug = false;
 
     game.physics.p2.updateBoundsCollisionGroup(); //UPDATE COLLISION BOUND FOR GROUPS
 
@@ -30,9 +26,7 @@ function Character(x, y, angle, game, player, asset)
     this.body.setCollisionGroup(game.spacePhysics.CG_teams[this.player.team.color - 1]);
     this.body.collideWorldBounds = true;
 
-
     //Attributes
-
     this.jumpForce = 75;
     this.health = 100;
     this.items = [];
@@ -43,10 +37,7 @@ function Character(x, y, angle, game, player, asset)
     this.turbos = 0;
     this.body.mass = 1
 
-
-
-    // Move
-
+    // Velocity in space
     this.moveSpaceThrust = 500;
 
     // States
@@ -79,18 +70,15 @@ function Character(x, y, angle, game, player, asset)
 
 
     // Damages
-
     this.damageAttack0 = 25;
     this.damageAttack1 = 17.5;
     this.damageAttack2 = 25;
     this.damageAttack3 = 10;
 
     // Multipliers
-
     this.turboMultiplier = 5;
 
     // Sounds
-
     this.attackSound = game.add.audio('pingas', 0.6, false);
     this.dieSound = game.add.audio('dieSound', 0.7, false);
     this.hitSound = game.add.audio('hitSound', 0.8, false);
@@ -99,46 +87,22 @@ function Character(x, y, angle, game, player, asset)
 
     var PTM = 50;
     var driveJoints = [];
-
-
     var frequency = 12;
     var damping = 100;
     var motorTorque = 1;
     var rideHeight = 0.25;
 
-
     this.planetTouched = null;
     this.atmosphere = [];
     this.grounded = false;
+    this.driveJoints = [];
+    this.wheels = [];
 
     //    var truckVertices = [-10, -30, 10, -30, 20, 0, -20, 0];
-
     //    this.body.setPolygon(truckVertices);
     this.body.mass = 1;
     this.body.angularDamping = 0.15;
     this.body.linearDamping = 0.3;
-
-    //    // Atributes while grounded
-    //    this.angularDampingGrounded = 0.15;
-    //    this.speedGrounded = 30;
-    //
-    //    // Atributes while not grounded in orbit
-    //    this.angularDampingOrbit = 0.15;
-    //    this.forceOrbit = 700;
-    //    this.angularSpeedOrbit = 150;
-    //
-    //    // Atributes in space
-    //    this.angularDampingSpaceRotating = 0.15;
-    //    this.angularDampingSpaceMoving = 10;
-    //    this.angularDampingSpaceStill = 0.15;
-    //    this.rotateSpace = 100;
-    //    this.angularVelocitySpace = 0.15;
-    //    this.angularSpeedSpace = 150;
-    //    this.forceSpace = 250;
-
-
-    this.driveJoints = [];
-    this.wheels = [];
 
     if (asset === 'playerRed')
     {
@@ -148,40 +112,25 @@ function Character(x, y, angle, game, player, asset)
     {
         this.wheel = 'wheel_blue';
     }
+
     this.wheels[0] = new Phaser.Sprite(game, this.x + -0.22 * PTM, this.y + 0.3 * -PTM, this.wheel);
     this.wheels[0].anchor.set(0.5);
     game.add.existing(this.wheels[0]);
+
     this.wheels[1] = new Phaser.Sprite(game, this.x + 0.22 * PTM, this.y + 0.3 * -PTM, this.wheel);
     this.wheels[1].anchor.set(0.5);
     game.add.existing(this.wheels[1]);
 
     // Bodies of the wheels
-
     this.game.physics.p2.enable([this.wheels[0], this.wheels[1]], this.debug);
-
-    //    this.wheels[0].body = new Phaser.Physics.Box2D.Body(this.game, null, this.x + -0.22 * PTM, this.y + 0.6 * -PTM, 1);
-    //    this.wheels[0].body = new Phaser.Physics.P2.Body(this.game, this.wheels[0], this.x + -0.22 * PTM, this.y + 0.6 * -PTM, 1);
-    //    this.wheels[0].body.sprite = this.wheels[0];
-    //    this.wheels[1].body = new Phaser.Physics.Box2D.Body(this.game, null, this.x + 0.22 * PTM, this.y + 0.6 * -PTM);
-    //    this.wheels[1].body.sprite = this.wheels[1];
-    //    this.wheels[1].body = new Phaser.Physics.P2.Body(this.game, this.wheels[1], this.x + -0.22 * PTM, this.y + 0.6 * -PTM, 1);
     this.wheels[0].body.setCircle(0.2 * PTM);
     this.wheels[1].body.setCircle(0.2 * PTM);
-    //    this.wheels[0].body.collideWorldBounds = true;
-    //    this.wheels[1].body.collideWorldBounds = true;
-    //    this.wheels[0].body.friction = 0.8;
-    //    this.wheels[1].body.friction = 0.8;
-    //    this.motorEnabled = false;
-    //    this.motorSpeed = 30;
 
+    // Wheel's properties
     var frequency = 3.5;
     var damping = 0.5;
     var motorTorque = 2;
     var rideHeight = 0.5;
-
-    // Joints between the wheels and the main body
-    //    this.driveJoints[0] = game.physics.box2d.wheelJoint(this.body, this.wheels[0].body, -0.65 * PTM, rideHeight * PTM, 0, 0, 0, 1, frequency, damping, 0, motorTorque, true);
-    //    this.driveJoints[1] = game.physics.box2d.wheelJoint(this.body, this.wheels[1].body, 0.65 * PTM, rideHeight * PTM, 0, 0, 0, 1, frequency, damping, 0, motorTorque, true);
 
     this.springs = [];
     this.springs[0] = game.physics.p2.createSpring(this, this.wheels[0], 70, 150, 50, null, null, [30, 0], null);
@@ -190,17 +139,18 @@ function Character(x, y, angle, game, player, asset)
 
     this.constraint = game.physics.p2.createPrismaticConstraint(this, this.wheels[0], false, [30, 0], [0, 0], [0, 1]);
 
-    //SET LIMITS
+    // Set limits left wheel
+    this.constraint_1.lowerLimitEnabled = this.constraint_1.upperLimitEnabled = true;
+    this.constraint_1.upperLimit = -0.7;
+    this.constraint_1.lowerLimit = -1;
+
+    // Set limits right wheel
     this.constraint.lowerLimitEnabled = this.constraint.upperLimitEnabled = true;
     this.constraint.upperLimit = -0.7;
     this.constraint.lowerLimit = -1;
     this.constraint_1 = game.physics.p2.createPrismaticConstraint(this, this.wheels[1], false, [-30, 0], [0, 0], [0, 1]);
 
-    //SET LIMITS
-    this.constraint_1.lowerLimitEnabled = this.constraint_1.upperLimitEnabled = true;
-    this.constraint_1.upperLimit = -0.7;
-    this.constraint_1.lowerLimit = -1;
-
+    // Collisions
     this.wheels[0].body.setCollisionGroup(game.spacePhysics.CG_teams[this.player.team.color - 1]);
     this.wheels[1].body.setCollisionGroup(game.spacePhysics.CG_teams[this.player.team.color - 1]);
 
@@ -220,7 +170,7 @@ function Character(x, y, angle, game, player, asset)
 
     this.body.collides(game.spacePhysics.CG_stageItems);
 
-
+    // Characters only collide with enemies
     for (var t in game.spacePhysics.CG_teams)
     {
         if (game.spacePhysics.CG_teams[t] === game.spacePhysics.CG_teams[this.player.team.color - 1])
@@ -231,13 +181,6 @@ function Character(x, y, angle, game, player, asset)
         this.wheels[0].body.collides(game.spacePhysics.CG_teams[t]);
         this.wheels[1].body.collides(game.spacePhysics.CG_teams[t]);
     }
-
-
-    //    this.body.onEndContact.add(untouchPlanetCallback, this);
-    //    this.wheels[0].body.onEndContact.add(untouchPlanetCallback, this.wheels[0]);
-    //    this.wheels[1].body.onEndContact.add(untouchPlanetCallback, this.wheels[1]);
-
-
 
     // This is for the collision callback
     this.wheels[0].body.mainSprite = this;
@@ -251,15 +194,12 @@ function Character(x, y, angle, game, player, asset)
     this.animations.add('jumpL', [4]);
     this.animations.add('jumpR', [5]);
 
-   // var bloodAnimation = new Animation(game, this, 'bloodEffect', "blood", [0,1,2,3], 20, false, false);
-   // var bloodEffect = game.add.sprite(300, 200, 'blood');
-
+    // Blood effect when damaging
     this.bloodEffect = this.addChild(game.make.sprite(0, 0, 'blood'));
     this.bloodEffect.anchor.set(0.5);
-    this.bloodEffect.animations.add('bleeding', [0,1,2,3,4,5]);
-    //this.bloodEffect.animations.stop();
-    //Display player name
+    this.bloodEffect.animations.add('bleeding', [0, 1, 2, 3, 4, 5]);
 
+    // Display player name
     var text = game.add.text(0, -56, player.nickname,
     {
         font: "16px Arial",
@@ -268,6 +208,7 @@ function Character(x, y, angle, game, player, asset)
     this.addChild(text);
     text.anchor.set(0.5);
 
+    // Material of the character's bodies
     this.body.setMaterial(game.spacePhysics.spriteMaterial);
     this.wheels[0].body.setMaterial(game.spacePhysics.wheelMaterial);
     this.wheels[1].body.setMaterial(game.spacePhysics.wheelMaterial);
@@ -282,10 +223,10 @@ function Character(x, y, angle, game, player, asset)
 
 Character.prototype.die = function ()
 {
-
     if (!this.alive) return;
 
     this.dieSound.play();
+    //      PARTICLES COMMITED BECAUSE OF A BUG
     //    var emitter = game.add.emitter(0, 0, 100);
     //    emitter.makeParticles('pokeball');
     //    emitter.x = this.x;//    emitter.y = this.y;
@@ -314,8 +255,6 @@ Character.prototype.die = function ()
             this.mines[m].die();
         }
     }
-    //    if (this.spikeball) this.spikeball.die();
-    //this.body.destroy();
 
     this.destroy();
 
@@ -323,34 +262,22 @@ Character.prototype.die = function ()
 
 Character.prototype.respawn = function (x, y, angle)
 {
-
     if (this.alive) return;
 
-    //    this.dieSound.play();
-    //    var emitter = game.add.emitter(0, 0, 100);
-    //    emitter.makeParticles('pokeball');
-    //    emitter.x = this.x;
-    //    emitter.y = this.y;
-    //    emitter.start(true, 2000, null, 10);
-    //    game.time.events.add(2000, function () {
-    //        if (emitter) emitter.destroy();
-    //    }, this);
-
-    //    this.revive(100);
     this.reset(x, y);
 
     this.body.rotation = angle;
     this.wheels[0].revive();
     this.wheels[1].revive();
-    this.constraint = game.physics.p2.createPrismaticConstraint(this, this.wheels[0], false, [30, 0], [0, 0], [0, 1]);
 
-    //SET LIMITS
+    // Set limits left wheel
+    this.constraint = game.physics.p2.createPrismaticConstraint(this, this.wheels[0], false, [30, 0], [0, 0], [0, 1]);
     this.constraint.lowerLimitEnabled = this.constraint.upperLimitEnabled = true;
     this.constraint.upperLimit = -0.7;
     this.constraint.lowerLimit = -1;
-    this.constraint_1 = game.physics.p2.createPrismaticConstraint(this, this.wheels[1], false, [-30, 0], [0, 0], [0, 1]);
 
-    //SET LIMITS
+    // Set limits right wheel
+    this.constraint_1 = game.physics.p2.createPrismaticConstraint(this, this.wheels[1], false, [-30, 0], [0, 0], [0, 1]);
     this.constraint_1.lowerLimitEnabled = this.constraint_1.upperLimitEnabled = true;
     this.constraint_1.upperLimit = -0.7;
     this.constraint_1.lowerLimit = -1;
@@ -362,7 +289,6 @@ Character.prototype.respawn = function (x, y, angle)
 
 Character.prototype.simpleDie = function ()
 {
-
     if (!this.alive) return;
     game.physics.p2.removeConstraint(this.constraint)
     game.physics.p2.removeConstraint(this.constraint_1)
@@ -384,18 +310,16 @@ Character.prototype.simpleDie = function ()
             this.mines[m].die();
         }
     }
-    //    if (this.spikeball) this.spikeball.die();
-    //this.body.destroy();
+
     this.health = 0;
     this.destroy();
-
 
 }
 
 
 Character.prototype.hurt = function (damage)
 {
-    this.bloodEffect.animations.play('bleeding',15,false);
+    this.bloodEffect.animations.play('bleeding', 15, false);
     this.health -= damage;
     this.hitSound.play();
 
@@ -423,7 +347,10 @@ Character.prototype.hurt = function (damage)
         this.hitImmune = true;
         game.time.events.add(this.hitImmuneTime, function ()
         {
-            if (this) { this.hitImmune = false; }
+            if (this)
+            {
+                this.hitImmune = false;
+            }
         }, this);
     }
 }
@@ -434,7 +361,6 @@ Character.prototype.hurt = function (damage)
 
 Character.prototype.inWorldCustom = function ()
 {
-
     return this.x >= 0 &&
         this.y >= 0 &&
         this.x <= game.world.x &&
